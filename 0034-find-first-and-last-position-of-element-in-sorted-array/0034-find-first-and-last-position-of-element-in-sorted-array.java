0f98eff3944f2eac8d83b6a3c575d8df43fa865e
siddharthmehta06 <siddharthsmwork@gmail.com>
2026-08-01 18:59:48 +0530

    Time: 0 ms (100%), Space: 48.2 MB (32.42%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,20 +1,56 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
+    public int[] searchRange(int[] nums, int target){
+           int[] result = {-1,-1};
 
-        int[] result = {-1, -1};
+            result[0]=findFirst(nums,target);
+            if(result[0]==-1){
+                return result;
+            } 
+            result[1]=findLast(nums,target);
+            return result;
+    }
 
-        for (int i = 0; i < nums.length; i++) {
+    public int findFirst(int[] nums, int target){
+        int low=0;
+        int high=nums.length-1;
+        int ans=-1;
+        
 
-            if (nums[i] == target) {
-
-                if (result[0] == -1) {
-                    result[0] = i;   // first occurrence
-                }
-
-                result[1] = i;      
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                high=mid-1;
+            }
+            if(nums[mid]>target){
+                high=mid-1;
+            }
+            if(nums[mid]<target){
+                low=mid+1;
             }
         }
+        return ans;
+    }
 
-        return result;
+    public int findLast(int[] nums, int target){
+        int low=0;
+        int high=nums.length-1;
+        int ans=-1;
+        
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                low=mid+1;
+            }
+            if(nums[mid]>target){
+                high=mid-1;
+            }
+            if(nums[mid]<target){
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 }
